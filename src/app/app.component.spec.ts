18b5e4ed91ed36d4363de7498b9b6acdfb07a230
Leonardo Aquino <laquino@soaint.com>
2024-09-05 18:10:08 -0500

    Chore: Se agregan configuraciones para el uso de Jest y SonarQube.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-let component: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
 
 describe('Suite AppComponent', () => {
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
+  it('should create the app', () => {
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'angular-testing-with-jest'`, () => {
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angular-testing-with-jest');
+  });
+
+  it('should render title', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'angular-testing-with-jest'
+    );
   });
 });
